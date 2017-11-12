3b64eaea7e3ac0bdd35f0176214e78b7ea3f23ad
Luis Troya <troyaluis56@gmail.com>
2017-11-12 17:41:54 -0400

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,6 @@ const socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to server');
-
-    socket.emit('createEmail', {
-        to: 'address@gmail.com',
-        text: 'Hey, this is Luis Troya'
-    });
-
-    socket.emit('createMessage', {
-        from: 'Luis Troya',
-        text: 'Hello World!'
-    })
 });
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -14,18 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.on('createMessage', (message) => {
-       console.log('Message', message);
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime(),
+    });
 
-       socket.emit('newMessage', {
-           from: message.from,
-           text: message.text,
-           createdAt: new Date()
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime(),
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('Message', message);
+        
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime(),
         });
     });
 
     socket.on('disconnect', () => {
-       console.log('User was disconnected');
+        console.log('User was disconnected');
     });
 });
 
